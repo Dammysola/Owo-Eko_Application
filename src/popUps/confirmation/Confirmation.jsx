4c387153e152a1ccf3529b8ad99810ca50ddd2b4
David Adeshina <dadeshina4@gmail.com>
2024-05-27 15:34:54 +0100

    Dark Dave 3

--- src/popUps/confirmation/Confirmation.jsx
+++ src/popUps/confirmation/Confirmation.jsx
@@ -1,38 +1,42 @@
 import React from 'react'
 import Style from '../confirmation/Confirmation.module.css'
-import Button from '../components/button/Button'
+import Button from '../../components/button/Button'
 
 
 const Confirmation = () => {
-  return (
-    <div id={Style.Confirmation_mainDiv}>
-        <div id={Style.Confirmation_Wrapper}>
-            <b id={Style.Confirmation_text}>Confirmation</b>
-            <table>
-                <thead>
-                    <td>Withdraw</td>
-                    <td><span>All Coins</span></td>
-                </thead>
-                <tr>
-                    <td>To</td>
-                    <td>John Doe</td>
-                </tr>
-                <tr>
-                    <td>Bank</td>
-                    <td>KudaBank</td>
-                </tr>
-                <td>Account</td>
-                <td>123456789</td>
-            </table>
-            <div>
-                <button id={Style.button}>Cancel</button>
-            <Button
-              type={"submit"}
-              text={"withdraw"}/>
+    return (
+        <div id={Style.Confirmation_mainDiv}>
+            <div id={Style.Confirmation_Wrapper}>
+                <b id={Style.Confirmation_text}>Confirmation</b>
+                <table>
+                    <thead>
+                        <td>Withdraw</td>
+                        <td ><span>All Coins</span></td>
+                    </thead>
+                    <tr>
+                        <td>To</td>
+                        <td className={Style.fadded}>John Doe</td>
+                    </tr>
+                    <tr>
+                        <td>Bank</td>
+                        <td className={Style.fadded}>KudaBank</td>
+                    </tr>
+                    <tr>
+
+                        <td>Account</td>
+                        <td className={Style.fadded}>123456789</td>
+                    </tr>
+                </table>
+                <div>
+                    <button id={Style.button}>Cancel</button>
+                    <Button
+                        type={"submit"}
+                        text={"Withdraw"}
+                    />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Confirmation
