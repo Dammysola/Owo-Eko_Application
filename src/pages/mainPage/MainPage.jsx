aa5d4f147aa6cf5a4773a2a807b787a298c94cd3
David Adeshina <dadeshina4@gmail.com>
2024-05-31 14:58:51 +0100

    Update MainPage.jsx

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -77,152 +77,152 @@ const MainPage = () => {
     //   link: "https://www.highcpmgate.com/jihvm3ui?key=f808f400599295c77be7e8ffc7c042a9",
     // },
     //Moneytag
-    {
-      text: "Money Tag - 1",
-      link: "https://meenetiy.com/4/7549769",
-    },
-    {
-      text: "Money Tag - 2",
-      link: "https://eetognauy.net/4/5114081",
-    },
-    {
-      text: "Money Tag - 3",
-      link: "https://tauphaub.net/4/7549785",
-    },
-    {
-      text: "Money Tag - 4",
-      link: "https://voostaidoo.net/4/7549778",
-    },
-    {
-      text: "Money Tag - 5",
-      link: "https://psoansumt.net/4/7549780",
-    },
-    {
-      text: "Money Tag - 6",
-      link: "https://oulsools.com/4/7549783",
-    },
-    {
-      text: "Money Tag - 7",
-      link: "https://owhaptih.net/4/7549787",
-    },
-    {
-      text: "Money Tag - 8",
-      link: "https://psoansumt.net/4/7549788",
-    },
-    {
-      text: "Money Tag - 9",
-      link: "https://shasogna.com/4/7549779",
-    },
-    {
-      text: "Money Tag - 10",
-      link: "https://outnidorinoom.com/4/7549786",
-    },
-    {
-      text: "Money Tag - 11",
-      link: "https://chouthep.net/4/7549772",
-    },
-    {
-      text: "Money Tag - 12",
-      link: "https://upontogeticr.com/4/7549784",
-    },
-    {
-      text: "Money Tag - 13",
-      link: "https://ethaistoothi.com/4/7549825",
-    },
-    {
-      text: "Money Tag - 14",
-      link: "https://psuftoum.com/4/7549833",
-    },
-    {
-      text: "Money Tag - 15",
-      link: "https://taucaphoful.net/4/7549836",
-    },
-    {
-      text: "Money Tag - 16",
-      link: "https://roastoup.com/4/7549838",
-    },
-    {
-      text: "Money Tag - 17",
-      link: "https://wissoony.net/4/7549841",
-    },
-    {
-      text: "Money Tag - 18",
-      link: "https://thechoansa.com/4/7549850",
-    },
-    {
-      text: "Money Tag - 19",
-      link: "https://loaptaijuw.com/4/7549851",
-    },
-    {
-      text: "Money Tag - 20",
-      link: "https://whulsaux.com/4/7549852",
-    },
+    // {
+    //   text: "Money Tag - 1",
+    //   link: "https://meenetiy.com/4/7549769",
+    // },
+    // {
+    //   text: "Money Tag - 2",
+    //   link: "https://eetognauy.net/4/5114081",
+    // },
+    // {
+    //   text: "Money Tag - 3",
+    //   link: "https://tauphaub.net/4/7549785",
+    // },
+    // {
+    //   text: "Money Tag - 4",
+    //   link: "https://voostaidoo.net/4/7549778",
+    // },
+    // {
+    //   text: "Money Tag - 5",
+    //   link: "https://psoansumt.net/4/7549780",
+    // },
+    // {
+    //   text: "Money Tag - 6",
+    //   link: "https://oulsools.com/4/7549783",
+    // },
+    // {
+    //   text: "Money Tag - 7",
+    //   link: "https://owhaptih.net/4/7549787",
+    // },
+    // {
+    //   text: "Money Tag - 8",
+    //   link: "https://psoansumt.net/4/7549788",
+    // },
+    // {
+    //   text: "Money Tag - 9",
+    //   link: "https://shasogna.com/4/7549779",
+    // },
+    // {
+    //   text: "Money Tag - 10",
+    //   link: "https://outnidorinoom.com/4/7549786",
+    // },
+    // {
+    //   text: "Money Tag - 11",
+    //   link: "https://chouthep.net/4/7549772",
+    // },
+    // {
+    //   text: "Money Tag - 12",
+    //   link: "https://upontogeticr.com/4/7549784",
+    // },
+    // {
+    //   text: "Money Tag - 13",
+    //   link: "https://ethaistoothi.com/4/7549825",
+    // },
+    // {
+    //   text: "Money Tag - 14",
+    //   link: "https://psuftoum.com/4/7549833",
+    // },
+    // {
+    //   text: "Money Tag - 15",
+    //   link: "https://taucaphoful.net/4/7549836",
+    // },
+    // {
+    //   text: "Money Tag - 16",
+    //   link: "https://roastoup.com/4/7549838",
+    // },
+    // {
+    //   text: "Money Tag - 17",
+    //   link: "https://wissoony.net/4/7549841",
+    // },
+    // {
+    //   text: "Money Tag - 18",
+    //   link: "https://thechoansa.com/4/7549850",
+    // },
+    // {
+    //   text: "Money Tag - 19",
+    //   link: "https://loaptaijuw.com/4/7549851",
+    // },
+    // {
+    //   text: "Money Tag - 20",
+    //   link: "https://whulsaux.com/4/7549852",
+    // },
 
     //Adsterra Gamer
+    {
+      text: "Adsterra Gamer - 1",
+      link: "https://www.highcpmgate.com/cfwz4zka?key=15e4796ca3c0cde02221ef4226684e34",
+    },
+    {
+      text: "Adsterra Gamer - 2",
+      link: "https://www.highcpmgate.com/c9rarvzh5?key=42b162bd11f747a51fa51a964d4d86a6",
+    },
+    {
+      text: "Adsterra Gamer - 3",
+      link: "https://www.highcpmgate.com/kqff7ke7?key=962c969075f2247812821961589e0400",
+    },
+    {
+      text: "Adsterra Gamer - 4",
+      link: "https://www.highcpmgate.com/jkqedxz0r?key=99587ee47cb6e97f1ea31bf0bbdd5c91",
+    },
+    {
+      text: "Adsterra Gamer - 5",
+      link: "https://www.highcpmgate.com/fj5878ge?key=90c95f41a246a762d22e8e1c217b440a",
+    },
+    {
+      text: "Adsterra Gamer - 6",
+      link: "https://www.highcpmgate.com/me02hvrig?key=f1157b70797165df316f20c8fec84f51",
+    },
+    {
+      text: "Adsterra Gamer - 7",
+      link: "https://www.highcpmgate.com/p8k4zkptn?key=3fecb3051ebe9c36247c45c029f705e3",
+    },
+    {
+      text: "Adsterra Gamer - 8",
+      link: "https://www.highcpmgate.com/f3mb4amav?key=decd3e887e3f800960e7d6b62785a398",
+    },
+    {
+      text: "Adsterra Gamer - 9",
+      link : "https://www.highcpmgate.com/ptppavw2pe?key=81034d6b489066e17d22418c2d705e23"
+    },
+    {
+      text: "Adsterra Gamer - 10",
+      link : "https://www.highcpmgate.com/t7k2f3m194?key=98c92bd9b4ae932e16bd6f6dfa25d3a7"
+    },
+    {
+      text: "Adsterra Gamer - 11",
+      link : "https://curvedheldideal.com/x5mu4ubb53?key=909b40894b733aab10f5904b0547c6e5"
+    },
+
+    // //Adsterra Unique
     // {
-    //   text: "22",
-    //   link: "https://www.highcpmgate.com/cfwz4zka?key=15e4796ca3c0cde02221ef4226684e34",
-    // },
-    // {
-    //   text: "23",
-    //   link: "https://www.highcpmgate.com/c9rarvzh5?key=42b162bd11f747a51fa51a964d4d86a6",
-    // },
-    // {
-    //   text: "24",
-    //   link: "https://www.highcpmgate.com/kqff7ke7?key=962c969075f2247812821961589e0400",
-    // },
-    // {
-    //   text: "25",
-    //   link: "https://www.highcpmgate.com/jkqedxz0r?key=99587ee47cb6e97f1ea31bf0bbdd5c91",
-    // },
-    // {
-    //   text: "26",
-    //   link: "https://www.highcpmgate.com/fj5878ge?key=90c95f41a246a762d22e8e1c217b440a",
-    // },
-    // {
-    //   text: "27",
-    //   link: "https://www.highcpmgate.com/me02hvrig?key=f1157b70797165df316f20c8fec84f51",
-    // },
-    // {
-    //   text: "28",
-    //   link: "https://www.highcpmgate.com/p8k4zkptn?key=3fecb3051ebe9c36247c45c029f705e3",
-    // },
-    // {
-    //   text: "29",
-    //   link: "https://www.highcpmgate.com/f3mb4amav?key=decd3e887e3f800960e7d6b62785a398",
-    // },
-    // {
-    //   text: "30",
-    //   link : "https://www.highcpmgate.com/ptppavw2pe?key=81034d6b489066e17d22418c2d705e23"
-    // },
-    // {
-    //   text: "31",
-    //   link : "https://www.highcpmgate.com/t7k2f3m194?key=98c92bd9b4ae932e16bd6f6dfa25d3a7"
-    // },
-    // {
-    //   text: "32",
-    //   link : "https://curvedheldideal.com/x5mu4ubb53?key=909b40894b733aab10f5904b0547c6e5"
-    // },
-    // {
-    //   text: "33",
+    //   text: "Adsterra Unique - 1",
     //   link : "https://www.highcpmgate.com/eh07g9563i?key=b730c667ed11095d5e1beb9a8d9d5db3"
     // },
-
-    // //Adsterra Barnney
     // {
-    //   text: "34",
+    //   text: "Adsterra Unique - 2",
     //   link : "https://barelydonkeysteed.com/dgta4wqgz?key=e917ef02f1a460d6d242ec2475b13aee"
     // },
     // {
-    //   text: "35",
+    //   text: "Adsterra Unique - 3",
     //   link : "https://barelydonkeysteed.com/fvij3tnh7d?key=98b2d1b5f52e437fbfb347e841ab8889"
     // },
     // {
-    //   text: "36",
+    //   text: "Adsterra Unique - 4",
     //   link : "https://barelydonkeysteed.com/r2g5mi9c?key=cb220c6a3ed2414269768cb278c0655f"
     // },
     // {
-    //   text: "37",
+    //   text: "Adsterra Unique - 5",
     //   link : "https://barelydonkeysteed.com/up344ha6?key=57c3546b3ad2e285d18cde968f3ba85e"
     // }
   ]
